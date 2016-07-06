56be5022fb5df3fafb1ee7a7c4de88cf5e6f9d16
Zion Nimchuk <zionnimchuk@dhcp-a4-5e-60-c9-98-21.ubcvisitor.wifi.ubc.ca>
2016-07-06 15:53:48 -0700

    Bleh, ACTUALLY did gamemanager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public bool gameOver = false;
+	public string mainSceneName = "Main Scene";
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		SceneManager.LoadSceneAsync(mainSceneName, LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
