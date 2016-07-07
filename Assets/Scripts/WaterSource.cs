f7599fd4c93700a9f393dd18939edd9c7b1c8ad0
Zion Nimchuk <zionnimchuk@dhcp-a4-5e-60-c9-98-21.ubcvisitor.wifi.ubc.ca>
2016-07-06 18:22:31 -0700

    Added waterSource prefab.

---
+++ Assets/Scripts/WaterSource.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WaterSource : MonoBehaviour {
+	public float waterRadius = 5;
+	private Transform radiusIndicator;
+	// Use this for initialization
+	void Start () {
+		radiusIndicator = transform.GetChild(0);
+		radiusIndicator.localScale = new Vector3((float)waterRadius, 0.2f, (float)waterRadius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
