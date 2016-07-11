ac3cea3ea1dd75d7536674e9abbb0d8712945ae2
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-11 16:24:38 -0700

    Added Countdown timer functionality.

--- Assets/Scripts/Player/CountDown.cs
+++ Assets/Scripts/Player/CountDown.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class CountDown : MonoBehaviour {
-
+	public float maxTime = 600; //Because it is in seconds;
+	private float curTime;
+	private string prettyTime;
 	// Use this for initialization
 	void Start () {
-	
+		curTime = maxTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		curTime -= Time.deltaTime;
+		//Debug.Log(curTime);
+		prettyTime = Mathf.FloorToInt(curTime/60) + ":" + Mathf.FloorToInt((60*(curTime/60 - Mathf.Floor(curTime/60))));
+		Debug.Log(prettyTime);
 	}
 }
