bbf9f477f3dc318045520b8a52c909b8044402d2
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-11 16:29:47 -0700

    Update text on countdown timer GUI

--- Assets/Scripts/Player/CountDown.cs
+++ Assets/Scripts/Player/CountDown.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CountDown : MonoBehaviour {
 	public float maxTime = 600; //Because it is in seconds;
 	private float curTime;
 	private string prettyTime;
+	private Text text;
 	// Use this for initialization
 	void Start () {
 		curTime = maxTime;
+		text = gameObject.GetComponent( typeof(Text) ) as Text;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +18,8 @@ public class CountDown : MonoBehaviour {
 		curTime -= Time.deltaTime;
 		//Debug.Log(curTime);
 		prettyTime = Mathf.FloorToInt(curTime/60) + ":" + Mathf.FloorToInt((60*(curTime/60 - Mathf.Floor(curTime/60))));
-		Debug.Log(prettyTime);
+		//Debug.Log(prettyTime);
+		text.text = prettyTime;
+
 	}
 }
