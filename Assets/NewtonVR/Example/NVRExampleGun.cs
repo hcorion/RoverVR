7950885a267de9641e2b3a88b1fe4a9b127a7bd8
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-11 17:27:00 -0700

    Added an interaction point to the Neutron Detector, and added a fuction to the Neturon Detector script, which is supposed to activate the tool's fuction when the button is pressed.

--- Assets/NewtonVR/Example/NVRExampleGun.cs
+++ Assets/NewtonVR/Example/NVRExampleGun.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 namespace NewtonVR.Example
 {
-    public class NVRExampleGun : NVRInteractableItem
-    {
-        public GameObject BulletPrefab;
+	public class NVRExampleGun : NVRInteractableItem
+	{
+		public GameObject BulletPrefab;
 
-        public Transform FirePoint;
+		public Transform FirePoint;
 
-        public Vector3 BulletForce = new Vector3(0, 0, 500);
+		public Vector3 BulletForce = new Vector3 (0, 0, 500);
 
-        public override void UseButtonDown()
-        {
-            base.UseButtonDown();
+		public override void UseButtonDown ()
+		{
+			base.UseButtonDown ();
 
-            GameObject bullet = GameObject.Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.position;
-            bullet.transform.forward = FirePoint.forward;
+			GameObject bullet = GameObject.Instantiate (BulletPrefab);
+			bullet.transform.position = FirePoint.position;
+			bullet.transform.forward = FirePoint.forward;
 
-            bullet.GetComponent<Rigidbody>().AddRelativeForce(BulletForce);
+			bullet.GetComponent<Rigidbody> ().AddRelativeForce (BulletForce);
 
-            AttachedHand.TriggerHapticPulse(500);
-        }
-    }
+			AttachedHand.TriggerHapticPulse (500);
+		}
+	}
 }
--- Assets/Scripts/Tools/NeutronDetector.cs
+++ Assets/Scripts/Tools/NeutronDetector.cs
@@ -2,49 +2,60 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class NeutronDetector : MonoBehaviour
+namespace NewtonVR.Example
 {
-	public float scanDistance;
-	public string units;
-	public GameObject textObject;
-
-	Text content;
-
-	void Start ()
+	public class NeutronDetector : NVRInteractableItem
 	{
-		content = textObject.GetComponent<Text> ();
-		content.text = "";
-	}
+		public float scanDistance;
+		public string units;
+		public GameObject textObject;
 
-	void Update ()
-	{
-		if (Input.GetMouseButton (0)) {
-			content.text = GetMoistureContent ();
-		} else {
+		Text content;
+
+		void Start ()
+		{
+			content = textObject.GetComponent<Text> ();
 			content.text = "";
 		}
-	}
 
-	string GetMoistureContent ()
-	{
-		RaycastHit hit;
-		Vector3 forward = transform.TransformVector (Vector3.forward);
-		Ray ray = new Ray (transform.position, forward);
-		if (Physics.Raycast (ray, out hit, scanDistance)) {
-			WaterSource waterSrc = hit.collider.GetComponent<WaterSource> ();
-			if (waterSrc != null) {
-				float moisture = waterSrc.moistureContent / Vector3.Distance (hit.point, hit.transform.position);
-				Debug.DrawLine (hit.transform.position, hit.point, Color.red, 20, false);
-				if (moisture > 0) {
-					return moisture.ToString () + units;
+		void Update ()
+		{
+			/*if (Input.GetMouseButton (0)) {
+				content.text = GetMoistureContent ();
+			} else {
+				content.text = "";
+			}*/
+		}
+
+		public override void UseButtonDown ()
+		{
+			base.UseButtonDown ();
+			content.text = GetMoistureContent ();
+			AttachedHand.TriggerHapticPulse (500);
+			print ("Activated!");
+		}
+
+		string GetMoistureContent ()
+		{
+			RaycastHit hit;
+			Vector3 forward = transform.TransformVector (Vector3.forward);
+			Ray ray = new Ray (transform.position, forward);
+			if (Physics.Raycast (ray, out hit, scanDistance)) {
+				WaterSource waterSrc = hit.collider.GetComponent<WaterSource> ();
+				if (waterSrc != null) {
+					float moisture = waterSrc.moistureContent / Vector3.Distance (hit.point, hit.transform.position);
+					Debug.DrawLine (hit.transform.position, hit.point, Color.red, 20, false);
+					if (moisture > 0) {
+						return moisture.ToString () + units;
+					} else {
+						return ("None");
+					}
 				} else {
 					return ("None");
 				}
 			} else {
 				return ("None");
 			}
-		} else {
-			return ("None");
 		}
 	}
 }
