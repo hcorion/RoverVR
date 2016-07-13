d34df5461ce7402c593f58c05ad5ddf56dc3d52d
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-12 18:27:24 -0700

    Attempeted to fix the Selfie Stick loading screen. Attempted to add a laser system, via line rendering to the Neutron Detector.

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour
+{
+	public void ShootLaserFromTargetPosition (LineRenderer lineRenderer, Vector3 targetPosition, Vector3 direction, float length, float width)
+	{
+		Ray ray = new Ray (targetPosition, direction);
+		RaycastHit hit;
+		Vector3 endPosition = targetPosition + (length * direction);
+
+		if (Physics.Raycast (ray, out hit, length)) {
+			endPosition = hit.point;
+		}
+
+		lineRenderer.SetPosition (0, targetPosition);
+		lineRenderer.SetPosition (1, endPosition);
+	}
+}
--- Assets/Scripts/Tools/NeutronDetector.cs
+++ Assets/Scripts/Tools/NeutronDetector.cs
@@ -10,13 +10,33 @@ namespace NewtonVR.Example
 		public string units;
 		public GameObject textObject;
 
+		public LineRenderer lineRenderer;
+		public float laserWidth;
+		public LaserScript laserScript;
+
 		Text content;
 
 		new void Start ()
 		{
 			base.Start ();
+
+			Vector3[] initLaserPositions = new Vector3[2] {
+				Vector3.zero,
+				Vector3.zero
+			};
+
+			//lineRenderer.SetPosition (initLaserPositions);
+			lineRenderer.SetWidth (laserWidth, laserWidth);
+
 			content = textObject.GetComponent<Text> ();
 			content.text = "";
+		}
+
+		void Update ()
+		{
+			if (AttachedHand != null) {
+				//Disable Hand Modle
+			}
 		}
 
 		public override void UseButtonDown ()
@@ -29,13 +49,14 @@ namespace NewtonVR.Example
 		string GetMoistureContent ()
 		{
 			RaycastHit hit;
-			Vector3 forward = transform.TransformVector (Vector3.forward);
+			Vector3 forward = transform.TransformVector (Vector3.left);
 			Ray ray = new Ray (transform.position, forward);
 			if (Physics.Raycast (ray, out hit, scanDistance)) {
 				Debug.DrawRay (transform.position, forward, Color.red, 1, false);
 				WaterSource waterSrc = hit.collider.GetComponent<WaterSource> ();
 				if (waterSrc != null) {
-					float moisture = waterSrc.moistureContent / Vector3.Distance (hit.point, hit.transform.position);
+					float moisture = 1 / Vector3.Distance (hit.point, hit.transform.position);
+					laserScript.ShootLaserFromTargetPosition (lineRenderer, transform.position, forward, scanDistance, laserWidth);
 					Debug.DrawLine (hit.transform.position, hit.point, Color.blue, 20, false);
 					if (moisture > 0) {
 						return moisture.ToString ("F2") + units;
