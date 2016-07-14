2e7507a2514f897d19189fd3de015d8967d35d36
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-14 14:18:33 -0700

    Fixed rocks, added automatic rock instantiation

---
+++ Assets/Scripts/Environment/RockSpawning.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockSpawning : MonoBehaviour {
+	public Vector3 positionOne;
+	public Vector3 positionTwo;
+
+	public GameObject[] rocks;
+	public int rocksToSpawn = 500;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for( int i = 0; i <= rocksToSpawn; i++)
+		{
+			Instantiate(rocks[Random.Range(0, rocks.Length)], new Vector3(Random.Range(positionOne.x, positionTwo.x), Random.Range(positionOne.y, positionTwo.y), Random.Range(positionOne.z, positionTwo.z)), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
