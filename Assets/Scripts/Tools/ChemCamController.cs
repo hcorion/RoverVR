9050bbfe28eab7af3f880b16215c32ca9200a1ff
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-14 17:38:04 -0700

    Fixed Chem Cam and added Chem Lab.

--- Assets/Scripts/Tools/ChemCamController.cs
+++ Assets/Scripts/Tools/ChemCamController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 namespace NewtonVR
 {
 	public class ChemCamController : NVRInteractableItem
@@ -9,33 +10,31 @@ namespace NewtonVR
 
 				
 		// Use this for initialization
-		new void Start () 
+		new void Start ()
 		{
 			base.Start ();
 		}
-		new void Update () 
+
+		new void Update ()
 		{
-			base.Update();
+			base.Update ();
 		}
 
 		public override void UseButtonDown ()
 		{
 			//Raycasting to ground
 			RaycastHit hit;
-			if(Physics.Raycast(shootPoint.position, Vector3.forward, out hit, 3))
-			{
-				Debug.Log("The ChemCam hit " + hit.transform.name + "At a distance of " + hit.distance);
+			Vector3 forward = transform.TransformVector (Vector3.right);
+			Debug.DrawRay (shootPoint.position, forward, Color.red, 20, false);
+			if (Physics.Raycast (shootPoint.position, forward, out hit, 3)) {
+				Debug.Log ("The ChemCam hit " + hit.transform.name + "At a distance of " + hit.distance);
 				ObjectProperties objectProperties = hit.transform.GetComponent<ObjectProperties> ();
-				if( objectProperties != null)
-				{
-					Debug.Log("The current material is:" + objectProperties.getMaterial());
-				}
-				else
-					Debug.Log("This object has no ObjectProperties script.");
-			}
-			else
-			{
-				Debug.Log("The ChemCam didn't hit anything. Move closer or something isn't working.");
+				if (objectProperties != null) {
+					Debug.Log ("The current material is:" + objectProperties.getMaterial ());
+				} else
+					Debug.Log ("This object has no ObjectProperties script.");
+			} else {
+				Debug.Log ("The ChemCam didn't hit anything. Move closer or something isn't working.");
 			}
 
 		}
