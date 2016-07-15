89fd15f8962bae7db5f9276bd13841d82fc0ed69
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-14 17:14:42 -0700

    Worked on basic functionality for ChemCam

--- Assets/Scripts/Objects/ObjectProperties.cs
+++ Assets/Scripts/Objects/ObjectProperties.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ObjectProperties : MonoBehaviour
 {
+	//public readonly string[] Materials = {"", ""};
+	public enum materials { Aluminium, Copper };
+	public materials materialSelect;
+
 	void Start ()
 	{
-		
 	}
 
 	void Update ()
 	{
-		
+
+	}
+	public string getMaterial()
+	{
+		return materialSelect.ToString();
 	}
 }
---
+++ Assets/Scripts/Tools/ChemCamController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace NewtonVR
+{
+	public class ChemCamController : NVRInteractableItem
+	{
+
+		public Transform shootPoint;
+
+				
+		// Use this for initialization
+		new void Start () 
+		{
+			base.Start ();
+		}
+		new void Update () 
+		{
+			base.Update();
+		}
+
+		public override void UseButtonDown ()
+		{
+			//Raycasting to ground
+			RaycastHit hit;
+			if(Physics.Raycast(shootPoint.position, Vector3.forward, out hit, 3))
+			{
+				Debug.Log("The ChemCam hit " + hit.transform.name + "At a distance of " + hit.distance);
+				ObjectProperties objectProperties = hit.transform.GetComponent<ObjectProperties> ();
+				if( objectProperties != null)
+				{
+					Debug.Log("The current material is:" + objectProperties.getMaterial());
+				}
+				else
+					Debug.Log("This object has no ObjectProperties script.");
+			}
+			else
+			{
+				Debug.Log("The ChemCam didn't hit anything. Move closer or something isn't working.");
+			}
+
+		}
+	}
+}
