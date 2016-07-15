5ae57d37b6aad8bb63cf69c6dbd61aec6a56d912
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-14 18:25:16 -0700

    Created a basic BBQ and it's basic script. P.S. the BBQ is the Chem Lab.

---
+++ Assets/Scripts/Tools/ChemLab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChemLab : MonoBehaviour
+{
+	public GameObject activationPlate;
+	public GameObject textObject;
+
+	Text elements;
+
+	void Start ()
+	{
+		elements = textObject.GetComponent <Text> ();
+		elements.text = "";
+	}
+
+	void OnCollisionEnter (Collider c)
+	{
+		
+	}
+}
