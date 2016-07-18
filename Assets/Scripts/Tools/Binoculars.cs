02256aebbce3163482d51b2af0c13e0c7ffc5b4d
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-18 14:56:31 -0700

    Added Binoculars and it's script to the game, as well as a render texture, and a render textures folder.

---
+++ Assets/Scripts/Tools/Binoculars.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NewtonVR.Example
+{
+	public class Binoculars : NVRInteractableItem
+	{
+
+		public float distance;
+		public GameObject viewCamera;
+
+		void Update ()
+		{
+			Vector3 forward = transform.TransformVector (Vector3.back);
+			Vector3 cameraPosition = transform.position + forward * distance;
+
+			viewCamera.transform.position = cameraPosition;
+		}
+	}
+}
