29dd5e2a704fa591e1de2c2588ccb7fa73009d23
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-18 18:10:19 -0700

    Fixed merge

--- Assets/Scripts/Tools/ChemCamController.cs
+++ Assets/Scripts/Tools/ChemCamController.cs
@@ -5,24 +5,6 @@ namespace NewtonVR
 {
 	public class ChemCamController : NVRInteractableItem
 	{
-
-		public Transform shootPoint;
-		private bool buttonDown = true;
-		//Is used to tell if the user has changed and is now pointing at a different rock.
-		private GameObject lastRock;
-		public GameObject laser;
-		private Vector3 laserIntialPosition;
-		public Material laserMat;
-		public GameObject lightGameObject;
-		//For the manipulation of the color of light.
-		public Light light;
-		private Color32 lightColourToLerp;
-		private Color32 previousColour;
-		private float currentTime;
-		//For breaking the rocks
-		private float breakTime;
-
-<<<<<<< HEAD
         public Transform shootPoint;
         private bool buttonDown = true;
         //Is used to tell if the user has changed and is now pointing at a different rock.
@@ -39,7 +21,7 @@ namespace NewtonVR
         //For breaking the rocks
         private float breakTime;
         float rockbreakage = 0.0f;
-=======
+
 		// Use this for initialization
 		new void Start ()
 		{
@@ -48,7 +30,6 @@ namespace NewtonVR
 			//laserMat = laser.GetComponent<Renderer>().material;
 			//light = lightGameObject.GetComponent<Light>();
 			previousColour = light.color;
->>>>>>> 17d2ffa265a505e3be0d251a3a3ef5457c54e216
 
 		}
 
@@ -67,15 +48,6 @@ namespace NewtonVR
 						string rockMaterial = objectProperties.getSimpleMaterial ();
 						Debug.Log ("The current material is:" + rockMaterial);
 
-						//Updating the position of the laser and the light
-						laser.transform.localPosition = new Vector3 (hit.distance / 2 + 0.12f, 0, 0) + laserIntialPosition;
-						laser.transform.localScale = new Vector3 (laser.transform.localScale.x, hit.distance * 55, laser.transform.localScale.z);
-						lightGameObject.transform.position = new Vector3 (hit.point.x, hit.point.y, hit.point.z) - transform.right / 9.0f;
-						float rockbreakage = 0.0f;
-						if (lastRock == hit.transform.gameObject) {
-							//If we're still on the same rock.
-
-<<<<<<< HEAD
                         //Updating the position of the laser and the light
                         laser.transform.localPosition = new Vector3(hit.distance / 2 + 0.12f, 0, 0) + laserIntialPosition;
                         laser.transform.localScale = new Vector3(laser.transform.localScale.x, hit.distance * 55, laser.transform.localScale.z);
@@ -172,81 +144,4 @@ namespace NewtonVR
             }
         }
     }
-=======
-							if (rockbreakage != 0.0f) {
-								breakTime += Time.deltaTime;
-								if (breakTime >= rockbreakage) {
-									Debug.Log ("Rock has been broken");
-								}
-							} else {
-								Debug.Log ("Rock is unbreakable");
-							}
-                                
-						} else {
-							rockbreakage = objectProperties.getSimpleRockBreakage ();
-							breakTime = 0;
-							if (lastRock == null) {
-								//If we haven't hit anything yet.
-								laser.SetActive (true);
-							}
-							switch (rockMaterial) {
-							case "nil":
-								lightColourToLerp = new Color32 (100, 255, 0, 255);
-								break;
-							case "Aluminium":
-								lightColourToLerp = new Color32 (76, 88, 156, 255);
-								break;
-							case "Copper":
-								lightColourToLerp = new Color32 (111, 181, 109, 255);
-								break;
-							default:
-								{
-									Debug.LogError ("Woops, the material " + rockMaterial + " is not recognized by the ChemCamController Script.");
-									lightColourToLerp = Color.red;
-									break;
-								}
-							}
-						}
-					} else
-						Debug.Log ("This object has no ObjectProperties script.");
-					//lastRock = null;
-					lerpColor ();
-				} else {
-					Debug.Log ("The ChemCam didn't hit anything. Move closer or something isn't working.");
-				}
-			}
-		}
-
-		public override void UseButtonDown ()
-		{
-			buttonDown = true;
-		}
-
-		public override void UseButtonUp ()
-		{
-			buttonDown = false;
-			lastRock = null;
-			laser.SetActive (false);
-		}
-
-		private void lerpColor ()
-		{
-			Debug.Log ("TEST");
-			Debug.Log (light);
-			Debug.Log (lightColourToLerp);
-			const float lerpTime = 2;
-			if (currentTime / lerpTime < 1.0) {
-				//If we haven't acheived the goal.
-				currentTime += Time.deltaTime;
-				light.color = Color32.Lerp (previousColour, lightColourToLerp, currentTime / lerpTime);
-				laserMat.color = Color32.Lerp (previousColour, lightColourToLerp, currentTime / lerpTime);
-			} else if (lightColourToLerp != light.color) {
-				//If we're changing color
-				currentTime = 0.0f;
-				previousColour = light.color;
-				//Test
-			}
-		}
-	}
->>>>>>> 17d2ffa265a505e3be0d251a3a3ef5457c54e216
 }
