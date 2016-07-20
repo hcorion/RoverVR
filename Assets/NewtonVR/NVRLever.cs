891b8b00fe5cfc20ae2eec8cb0cbff716e7b70a3
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-19 19:08:42 -0700

    Added a spot light to the Neutron Detector. Worked on the Lever added the flare gun.

--- Assets/NewtonVR/NVRLever.cs
+++ Assets/NewtonVR/NVRLever.cs
@@ -1,152 +1,149 @@
+
 using UnityEngine;
 using System.Collections;
 using System;
 
 namespace NewtonVR
 {
-    public class NVRLever : NVRInteractable
-    {
-        public float LastValue;
-        public float CurrentValue;
-        public LeverPosition LastLeverPosition;
-        public LeverPosition CurrentLeverPosition;
-        public bool LeverEngaged = false;
-        public float EngageWaitTime = 1f;
+	public class NVRLever : NVRInteractable
+	{
+		public float LastValue;
+		public float CurrentValue;
+		public LeverPosition LastLeverPosition;
+		public LeverPosition CurrentLeverPosition;
+		public bool LeverEngaged = false;
+		public float EngageWaitTime = 1f;
 
-        protected virtual float DeltaMagic { get { return 2f; } }
-        protected Transform InitialAttachPoint;
-        protected HingeJoint HingeJoint;
+		protected virtual float DeltaMagic { get { return 2f; } }
 
-        protected bool UseMotor;
-        protected Quaternion Max, Mid, Min;
-        protected float AngleRange;
+		protected Transform InitialAttachPoint;
+		protected HingeJoint HingeJoint;
 
-        protected override void Awake()
-        {
-            base.Awake();
-            this.Rigidbody.maxAngularVelocity = 100f;
+		protected bool UseMotor;
+		protected Quaternion Max, Mid, Min;
+		protected float AngleRange;
 
-            if (HingeJoint == null)
-            {
-                HingeJoint = Rigidbody.gameObject.GetComponent<HingeJoint>();
-            }
+		protected override void Awake ()
+		{
+			base.Awake ();
+			this.Rigidbody.maxAngularVelocity = 100f;
 
-            Mid = HingeJoint.transform.localRotation;
-            Max = Mid * Quaternion.AngleAxis(HingeJoint.limits.max, HingeJoint.axis);
-            Min = Mid * Quaternion.AngleAxis(HingeJoint.limits.min, HingeJoint.axis);
-            UseMotor = this.HingeJoint.useMotor;
+			if (HingeJoint == null) {
+				HingeJoint = Rigidbody.gameObject.GetComponent<HingeJoint> ();
+			}
 
-            if (HingeJoint.useLimits)
-            {
-                AngleRange = (Mathf.Max(HingeJoint.limits.max, HingeJoint.limits.min) - Mathf.Min(HingeJoint.limits.max, HingeJoint.limits.min));
-            }
-        }
+			Mid = HingeJoint.transform.localRotation;
+			Max = Mid * Quaternion.AngleAxis (HingeJoint.limits.max, HingeJoint.axis);
+			Min = Mid * Quaternion.AngleAxis (HingeJoint.limits.min, HingeJoint.axis);
+			UseMotor = this.HingeJoint.useMotor;
+			if (HingeJoint.useLimits) {
+				AngleRange = (Mathf.Max (HingeJoint.limits.max, HingeJoint.limits.min) - Mathf.Min (HingeJoint.limits.max, HingeJoint.limits.min));
+				//AngleRange = 0;
+			}
+		}
 
-        public override void OnNewPosesApplied()
-        {
-            base.OnNewPosesApplied();
+		public override void OnNewPosesApplied ()
+		{
+			base.OnNewPosesApplied ();
 
-            if (IsAttached == true)
-            {
-                Vector3 PositionDelta = (AttachedHand.transform.position - InitialAttachPoint.position) * DeltaMagic;
+			if (IsAttached == true) {
+				Vector3 PositionDelta = (AttachedHand.transform.position - InitialAttachPoint.position) * DeltaMagic;
 
-                this.Rigidbody.AddForceAtPosition(PositionDelta, InitialAttachPoint.position, ForceMode.VelocityChange);
-            }
-        }
+				this.Rigidbody.AddForceAtPosition (PositionDelta, InitialAttachPoint.position, ForceMode.VelocityChange);
+			}
+		}
 
-        protected override void Update()
-        {
-            base.Update();
+		protected override void Update ()
+		{
+			base.Update ();
 
-            LeverEngaged = false;
-            LastValue = CurrentValue;
-            LastLeverPosition = CurrentLeverPosition;
+			LeverEngaged = false;
+			LastValue = CurrentValue;
+			LastLeverPosition = CurrentLeverPosition;
 
-            CurrentValue = GetValue();
-            CurrentLeverPosition = GetPosition();
+			CurrentValue = GetValue ();
+			CurrentLeverPosition = GetPosition ();
 
-            if (LastLeverPosition != LeverPosition.On && CurrentLeverPosition == LeverPosition.On)
-            {
-                LeverEngaged = true;
-                Engage();
-            }
-        }
+			if (LastLeverPosition != LeverPosition.On && CurrentLeverPosition == LeverPosition.On) {
+				LeverEngaged = true;
+				Engage ();
+			}
+		}
 
-        protected virtual void Engage()
-        {
-            if (AttachedHand != null)
-                AttachedHand.EndInteraction(this);
+		protected virtual void Engage ()
+		{
+			if (AttachedHand != null)
+				AttachedHand.EndInteraction (this);
 
-            CanAttach = false;
+			CanAttach = false;
 
-            StartCoroutine(HoldPosition(EngageWaitTime));
-        }
+			StartCoroutine (HoldPosition (EngageWaitTime));
+		}
 
-        private IEnumerator HoldPosition(float time)
-        {
-            HingeJoint.useMotor = false;
+		private IEnumerator HoldPosition (float time)
+		{
+			HingeJoint.useMotor = false;
 
-            yield return new WaitForSeconds(time);
+			yield return new WaitForSeconds (time);
 
-            HingeJoint.useMotor = true;
-            CanAttach = true;
-        }
+			HingeJoint.useMotor = true;
+			CanAttach = true;
+		}
 
-        public override void BeginInteraction(NVRHand hand)
-        {
-            base.BeginInteraction(hand);
+		public override void BeginInteraction (NVRHand hand)
+		{
+			base.BeginInteraction (hand);
 
-            InitialAttachPoint = new GameObject(string.Format("[{0}] InitialAttachPoint", this.gameObject.name)).transform;
-            InitialAttachPoint.position = hand.transform.position;
-            InitialAttachPoint.rotation = hand.transform.rotation;
-            InitialAttachPoint.localScale = Vector3.one * 0.25f;
-            InitialAttachPoint.parent = this.transform;
+			InitialAttachPoint = new GameObject (string.Format ("[{0}] InitialAttachPoint", this.gameObject.name)).transform;
+			InitialAttachPoint.position = hand.transform.position;
+			InitialAttachPoint.rotation = hand.transform.rotation;
+			InitialAttachPoint.localScale = Vector3.one * 0.25f;
+			InitialAttachPoint.parent = this.transform;
             
-            HingeJoint.useMotor = false;
-        }
+			HingeJoint.useMotor = false;
+		}
 
-        public override void EndInteraction()
-        {
-            base.EndInteraction();
+		public override void EndInteraction ()
+		{
+			base.EndInteraction ();
 
-            HingeJoint.useMotor = true;
+			HingeJoint.useMotor = true;
 
-            if (InitialAttachPoint != null)
-                Destroy(InitialAttachPoint.gameObject);
-        }
+			if (InitialAttachPoint != null)
+				Destroy (InitialAttachPoint.gameObject);
+		}
 
-        private float GetValue()
-        {
-            float m_diff = 0.0f;
-            if (HingeJoint.useLimits)
-            {
-                m_diff = HingeJoint.angle - HingeJoint.limits.min;
-            }
-            return 1 - (m_diff / AngleRange);
-        }
+		private float GetValue ()
+		{
+			float m_diff = 0.0f;
+			if (HingeJoint.useLimits) {
+				m_diff = HingeJoint.angle - HingeJoint.limits.min;
+			}
+			return 1 - (m_diff / AngleRange);
+		}
 
-        private LeverPosition GetPosition()
-        {
-            if (CurrentValue <= 0.05)
-                return LeverPosition.Off;
-            else if (CurrentValue >= 0.95)
-                return LeverPosition.On;
+		private LeverPosition GetPosition ()
+		{
+			if (CurrentValue <= 0.05)
+				return LeverPosition.Off;
+			else if (CurrentValue >= 0.95)
+				return LeverPosition.On;
 
-            return LeverPosition.Mid;
-        }
+			return LeverPosition.Mid;
+		}
 
-        public enum LeverPosition
-        {
-            Off,
-            Mid,
-            On
-        }
+		public enum LeverPosition
+		{
+			Off,
+			Mid,
+			On
+		}
 
-        public enum RotationAxis
-        {
-            XAxis,
-            YAxis,
-            ZAxis
-        }
-    }
+		public enum RotationAxis
+		{
+			XAxis,
+			YAxis,
+			ZAxis
+		}
+	}
 }
