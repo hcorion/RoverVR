c576193dd40b6926af6b7786e27c7be6d2c860ba
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-19 17:28:09 -0700

    Did some stuff with ObjectProps

---
+++ Assets/Scripts/Repairer/toolDetector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class toolDetector : MonoBehaviour {
+	public RepairerScript repairerScript;
+	public bool isToolDetector;
+	void OnTriggerEnter(Collider other)
+     {
+		 if(other.gameObject.GetComponent<toolProperties>() != null && isToolDetector == true)
+		 {
+		 	repairerScript.ToolTriggerEntered(other.gameObject.GetComponent<toolProperties>().getTool(), other);
+		 }
+		 else if (other.gameObject.tag == "" && isToolDetector == false)
+		 {
+			 repairerScript.rfMaterialTriggerEntered(other.gameObject);
+		 }
+     }
+}
---
+++ Assets/Scripts/RepairerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepairerScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToolTriggerEntered(string tool, Collider col)
+	{
+		switch (tool)
+		 {
+            case "SelfieStick":
+				Debug.Log("Selfie Stick has been added");
+				snapObject();
+				break;
+			case "NeutronDetector":
+				break;
+			case "Binoculars":
+				break;
+			case "ChemCam":
+				break;
+			default:
+				Debug.Log("The object " + tool + " is not supported for input.");
+				break;
+		 }
+	}
+
+	public void rfMaterialTriggerEntered(GameObject rfMaterial)
+	{
+		
+	}
+	private void snapObject()
+	{
+
+	}
+}
