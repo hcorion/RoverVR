c50989dbfc04d885ef75652fd09656fe85a2d5c4
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-19 21:03:03 -0700

    Added in a tool rack and the GrabTool.cs

--- Assets/Scripts/Tools/Binoculars.cs
+++ Assets/Scripts/Tools/Binoculars.cs
@@ -8,21 +8,10 @@ namespace NewtonVR.Example
 	{
 		public float distance;
 		public Camera viewCamera;
-		public GameObject textObject;
-		public GameObject origin;
-		public string units;
 
 		void Update ()
 		{
 			viewCamera.fieldOfView = distance;
-
-			RaycastHit hit;
-			Vector3 forward = transform.TransformVector (Vector3.back);
-			Ray ray = new Ray (origin.transform.position, forward);
-
-			if (Physics.Raycast (ray, out hit)) {
-				textObject.GetComponent<Text> ().text = hit.distance.ToString ("F2") + units;
-			}
 		}
 	}
 }
