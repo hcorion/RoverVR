83ea4550487755f0064be7608cafbde701e8bb85
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-19 18:17:10 -0700

    Added a distance meter to the binoculars.

--- Assets/Scripts/Tools/Binoculars.cs
+++ Assets/Scripts/Tools/Binoculars.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 namespace NewtonVR.Example
@@ -7,10 +8,21 @@ namespace NewtonVR.Example
 	{
 		public float distance;
 		public Camera viewCamera;
+		public GameObject textObject;
+		public GameObject origin;
+		public string units;
 
 		void Update ()
 		{
 			viewCamera.fieldOfView = distance;
+
+			RaycastHit hit;
+			Vector3 forward = transform.TransformVector (Vector3.back);
+			Ray ray = new Ray (origin.transform.position, forward);
+
+			if (Physics.Raycast (ray, out hit)) {
+				textObject.GetComponent<Text> ().text = hit.distance.ToString ("F2") + units;
+			}
 		}
 	}
 }
--- Assets/Scripts/Tools/ChemCamController.cs
+++ Assets/Scripts/Tools/ChemCamController.cs
@@ -58,6 +58,7 @@ namespace NewtonVR
                             
                                 if (rockbreakage != 0.0f)
                                 {
+																		GetComponent<AudioSource>().Play();
                                     breakTime += Time.deltaTime;
                                     if (breakTime >= rockbreakage)
                                     {
