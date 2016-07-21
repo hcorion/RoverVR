959d6cea6ffd93cb77c5ba007240838f51ce894a
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-20 20:58:05 -0700

    Moar debugging

--- Assets/Scripts/Repairer/toolDetector.cs
+++ Assets/Scripts/Repairer/toolDetector.cs
@@ -8,17 +8,19 @@ public class toolDetector : MonoBehaviour {
 		 Debug.Log("We made it here!");
 		 if(other.gameObject.GetComponent<ToolProperties>() != null)// && isToolDetector == true)
 		 {
+			 Debug.Log("The object " + other.gameObject.name + " is a tool");
 			 Debug.Log("I've hit a Tool!");
 		 	repairerScript.ToolTriggerEntered(other.gameObject.GetComponent<ToolProperties>().getTool(), other);
 		 }
 		 else if (other.gameObject.tag == "" && isToolDetector == false)
 		 {
-			 Debug.Log("I've hit  a rfMaterial!");
+			 Debug.Log("I've hit a rfMaterial!");
 			 repairerScript.rfMaterialTriggerEntered(other.gameObject);
 		 }
 		 else
 		 {
 			 Debug.Log("This object doesn't have toolProperties or isn't an ingot");
+			 Debug.Log("The object " + other.gameObject.name + " doesn't work.");
 		 }
      }
 }
