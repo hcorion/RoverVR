f00746875dfa047c439ae6efba4195c3180241b6
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-21 17:17:25 -0700

    Worked on ToolDetector

--- Assets/Scripts/Repairer/toolDetector.cs
+++ Assets/Scripts/Repairer/toolDetector.cs
@@ -5,14 +5,15 @@ public class toolDetector : MonoBehaviour {
 	public bool isToolDetector;
 	void OnTriggerEnter(Collider other)
      {
+		 GameObject otherRB = other.attachedRigidbody.gameObject;
 		 Debug.Log("We made it here!");
-		 if(other.gameObject.GetComponent<ToolProperties>() != null)// && isToolDetector == true)
+		 if(otherRB.GetComponent<ToolProperties>() != null)// && isToolDetector == true)
 		 {
-			 Debug.Log("The object " + other.gameObject.name + " is a tool");
+			 Debug.Log("The object " + otherRB.name + " is a tool");
 			 Debug.Log("I've hit a Tool!");
-		 	repairerScript.ToolTriggerEntered(other.gameObject.GetComponent<ToolProperties>().getTool(), other);
+		 	repairerScript.ToolTriggerEntered(otherRB.GetComponent<ToolProperties>().getTool(), other);
 		 }
-		 else if (other.gameObject.tag == "" && isToolDetector == false)
+		 else if (otherRB.tag == "" && isToolDetector == false)
 		 {
 			 Debug.Log("I've hit a rfMaterial!");
 			 repairerScript.rfMaterialTriggerEntered(other.gameObject);
@@ -20,7 +21,7 @@ public class toolDetector : MonoBehaviour {
 		 else
 		 {
 			 Debug.Log("This object doesn't have toolProperties or isn't an ingot");
-			 Debug.Log("The object " + other.gameObject.name + " doesn't work.");
+			 Debug.Log("The object " + otherRB.name + " doesn't work.");
 		 }
      }
 }
