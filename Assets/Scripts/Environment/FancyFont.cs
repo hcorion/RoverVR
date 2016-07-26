8f9dedc1584fdbc7beffa0ccfb281e2d1d624522
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-26 10:50:11 -0700

    Started work on fancy font

---
+++ Assets/Scripts/Environment/FancyFont.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FancyFont : MonoBehaviour {
+	public string fullText = "This is a Test";
+	private Text text;
+	private string[] words;
+
+	// Use this for initialization
+	void Awake()
+	{
+		text = gameObject.GetComponent<Text>();
+		if (text == null)
+			Debug.LogError("text is equal to null!");
+	}
+	void Start () {
+		text.text = "";
+		words = fullText.Split(' ');
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
