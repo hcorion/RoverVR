39c81119dd67095d27beea31949307d87bb673e4
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-26 13:09:02 -0700

    Added basic framework with GameManager script and fixed improved GameStart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 	public GameObject wonText;
 	public string mainSceneName = "Main Scene";
 	public int a = 0;
+	private bool hasStartedGame = false;
 
 	void Awake ()
 	{
@@ -19,6 +20,10 @@ public class GameManager : MonoBehaviour
 	void Start ()
 	{
 		//SceneManager.LoadSceneAsync(mainSceneName, LoadSceneMode.Single);
+		if(SceneManager.GetActiveScene().ToString() == mainSceneName)
+		{
+			firstState();
+		}
 	}
 
 	void Update ()
@@ -27,10 +32,26 @@ public class GameManager : MonoBehaviour
 			//Application.Quit();
 			wonText.SetActive (true);
 		}
+		if(SceneManager.GetActiveScene().ToString() == mainSceneName)
+		{
+			if(hasStartedGame == false)
+			{
+				firstState();
+				hasStartedGame = true;
+			}
+		}
 	}
 
 	public void GameOver ()
 	{
 		gameOver = true;
+	}
+	private void firstState()
+	{
+
+	}
+	public string getMainSceneName()
+	{
+		return mainSceneName;
 	}
 }
