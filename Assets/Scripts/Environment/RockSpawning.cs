0dc95eb140f9991184e56cb9e579c2c4e14e3569
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-27 18:54:27 -0700

    Added better rock Spawning Script

--- Assets/Scripts/Environment/RockSpawning.cs
+++ Assets/Scripts/Environment/RockSpawning.cs
@@ -4,22 +4,35 @@ using System.Collections;
 public class RockSpawning : MonoBehaviour {
 	public Vector3 positionOne;
 	public Vector3 positionTwo;
+	private float minTerrainHeight;
+
+	public GameObject cameraRig;
+
+	public Terrain terrain;
 
 	public GameObject[] rocks;
 	public int rocksToSpawn = 500;
-
+	void Awake()
+	{
+		minTerrainHeight = terrain.SampleHeight(cameraRig.transform.position);
+	}
 	// Use this for initialization
 	void Start () 
 	{
 		for( int i = 0; i <= rocksToSpawn; i++)
 		{
-			Instantiate(rocks[Random.Range(0, rocks.Length)], new Vector3(Random.Range(positionOne.x, positionTwo.x), Random.Range(positionOne.y, positionTwo.y), Random.Range(positionOne.z, positionTwo.z)), Quaternion.identity);
+			Vector3 randomPos = createRandomPos();
+			if (terrain.SampleHeight(randomPos) == minTerrainHeight)
+			{
+				Instantiate(rocks[Random.Range(0, rocks.Length)],randomPos, Quaternion.identity);
+			}
+			//Instantiate(rocks[Random.Range(0, rocks.Length)], new Vector3(Random.Range(positionOne.x, positionTwo.x), Random.Range(positionOne.y, positionTwo.y), Random.Range(positionOne.z, positionTwo.z)), Quaternion.identity);
 		}
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	Vector3 createRandomPos()
 	{
-	
+		return new Vector3(Random.Range(positionOne.x, positionTwo.x), minTerrainHeight + 1f, Random.Range(positionOne.x, positionTwo.x));
 	}
 }
