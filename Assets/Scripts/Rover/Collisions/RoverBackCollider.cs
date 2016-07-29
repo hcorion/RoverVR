2db8abf18258417453724ce4fa1f5dda61db08c6
Zion Nimchuk <zionnimchuk@gmail.com>
2016-07-28 20:13:08 -0700

    Worked on rover collisions.

---
+++ Assets/Scripts/Rover/Collisions/RoverBackCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoverBackCollider : MonoBehaviour
+{
+	public static bool canMoveBackward = true;
+
+	void OnTriggerEnter (Collider c)
+	{
+		canMoveBackward = false;
+	}
+
+	void OnTriggerExit (Collider c)
+	{
+		canMoveBackward = true;
+	}
+}
